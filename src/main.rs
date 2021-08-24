802bd0a7bc900338617382bab3f86f174aea30ed
Thierry Berger <contact@thierryberger.com>
2021-08-24 23:11:47 +0200

    cleaner ECS organisation

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,13 @@
-use std::string;
-
 use bevy::prelude::*;
 use bevy_egui::*;
-
-#[derive(Default)]
-pub struct GameState {
-    pub errors: Vec<egui::Pos2>,
-    pub success: usize,
-}
 
 mod GameButton {
     use super::*;
 
+    pub struct GeneralInput {
+        pub clicks: usize,
+        pub catched: usize,
+    }
     pub struct ButtonInfo {
         pub text: String,
         pub position: egui::Rect,
@@ -35,21 +31,70 @@ mod GameButton {
         Hidden(f32),
     }
 
+    pub struct MovingDef {
+        pub path: Vec<egui::Pos2>,
+        pub speed: f32,
+    }
+    pub struct MovingState {
+        pub target_index: usize,
+    }
+
+    // Translated from https://answers.unity.com/questions/414829/any-one-know-maths-behind-this-movetowards-functio.html
+    fn move_towards(current: egui::Pos2, target: egui::Pos2, max_distance: f32) -> egui::Pos2 {
+        let a = target - current;
+        let magnitude = a.length();
+        if magnitude <= max_distance || magnitude == 0f32 {
+            return target;
+        }
+        current + a / magnitude * max_distance
+    }
+    pub fn button_move(
+        time: Res<Time>,
+        mut q_buttons: Query<(&mut MovingDef, &mut MovingState, &mut ButtonInfo)>,
+    ) {
+        let delta_time = time.delta().as_secs_f32();
+        for (def, mut state, mut info) in q_buttons.iter_mut() {
+            let target = def.path[state.target_index];
+            let new_pos = move_towards(info.position.min, target, def.speed * delta_time);
+            info.position = egui::Rect::from_min_size(new_pos, info.position.size());
+            if new_pos == target {
+                state.target_index = (state.target_index + 1) % def.path.len();
+            }
+        }
+    }
+
     pub fn button_blink(
-        time: Res<&Time>,
+        time: Res<Time>,
         mut q_buttons: Query<(&mut Blinking, &mut BlinkState, &mut ButtonInfo)>,
     ) {
+        let elapsed_time = time.time_since_startup().as_secs_f32();
         for (def, mut state, mut info) in q_buttons.iter_mut() {
             match *state {
-                BlinkState::Visible(_) => todo!(),
-                BlinkState::Hidden(_) => todo!(),
+                BlinkState::Visible(v) => {
+                    if v <= elapsed_time {
+                        *state = BlinkState::Hidden(elapsed_time + def.hidden);
+                        info.visible = false;
+                    }
+                }
+                BlinkState::Hidden(h) => {
+                    if h <= elapsed_time {
+                        *state = BlinkState::Visible(elapsed_time + def.hidden);
+                        info.visible = true;
+                    }
+                }
             }
         }
     }
     pub fn button_letters(
+        general_input: Res<GeneralInput>,
         mut q_buttons: Query<(&mut LetterByLetter, &UserInput, &mut ButtonInfo)>,
     ) {
         for (mut letters, input, mut info) in q_buttons.iter_mut() {
+            if 0 < general_input.clicks && 0 == general_input.catched {
+                letters.current_index = 0;
+                info.text = letters.full_string.chars().take(1).collect::<String>();
+                // TODO: spawn entity to show "ILLUSION" as label for a short time
+            }
             if input.clicked_on_frame {
                 letters.current_index += 1;
                 info.text = letters
@@ -62,6 +107,7 @@ mod GameButton {
         }
     }
     pub fn display_buttons(
+        mut general_input: ResMut<GeneralInput>,
         egui_context: ResMut<EguiContext>,
         mut q_buttons: Query<(&ButtonInfo, &mut UserInput)>,
     ) {
@@ -70,34 +116,42 @@ mod GameButton {
             .show(egui_context.ctx(), |ui| {
                 for (info, mut input) in q_buttons.iter_mut() {
                     ui.allocate_ui_at_rect(info.position, |ui| {
-                        if ui.button(info.text.to_string()).is_pointer_button_down_on()
-                            && ui.input().pointer.any_pressed()
-                        {
+                        let button = if info.visible {
+                            ui.button(info.text.to_string())
+                        } else {
+                            ui.label(" ")
+                        };
+                        if button.is_pointer_button_down_on() && ui.input().pointer.any_pressed() {
                             input.clicked_on_frame = true;
+                            general_input.catched += 1;
                         }
                     });
                 }
+                if ui.input().pointer.any_pressed() {
+                    general_input.clicks = 1;
+                }
             });
     }
-    pub fn reset_input(mut q_buttons: Query<&mut UserInput>) {
+    pub fn reset_input(
+        mut general_input: ResMut<GeneralInput>,
+        mut q_buttons: Query<&mut UserInput>,
+    ) {
         for mut input in q_buttons.iter_mut() {
             input.clicked_on_frame = false;
         }
+        general_input.catched = 0;
+        general_input.clicks = 0;
     }
 }
 
-pub struct LevelDef {
-    pub buttons: Vec<(egui::Rect, String, usize, bool)>,
-    pub show_errors: bool,
-    pub error: String,
-    pub text_placeholder: String,
-    pub text_win: String,
-}
 use GameButton::*;
 fn main() {
     App::build()
         .insert_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)))
-        .insert_resource(GameState::default())
+        .insert_resource(GeneralInput {
+            clicks: 0,
+            catched: 0,
+        })
         .add_plugins(DefaultPlugins)
         .add_plugin(EguiPlugin)
         .add_startup_system(setup_level.system())
@@ -105,10 +159,12 @@ fn main() {
         .add_system_to_stage(CoreStage::PreUpdate, reset_input.system())
         .add_system_to_stage(CoreStage::Update, display_buttons.system())
         .add_system_to_stage(CoreStage::PostUpdate, button_letters.system())
+        .add_system_to_stage(CoreStage::PostUpdate, button_blink.system())
+        .add_system_to_stage(CoreStage::PostUpdate, button_move.system())
         .run();
 }
 
-fn setup_level(mut commands: Commands) {
+fn setup_level(time: Res<Time>, mut commands: Commands) {
     commands
         .spawn()
         .insert(ButtonInfo {
@@ -118,97 +174,19 @@ fn setup_level(mut commands: Commands) {
         })
         .insert(LetterByLetter {
             current_index: 0,
-            full_string: "Safety".into(),
+            full_string: "Security".into(),
         })
-        .insert(UserInput::default());
-    commands.insert_resource(LevelDef {
-        buttons: vec![
-            (
-                egui::Rect::from_min_size([100f32, 10f32].into(), [200f32, 50f32].into()),
-                "Reliability".into(),
-                0,
-                false,
-            ),
-            (
-                egui::Rect::from_min_size([100f32, 10f32].into(), [100f32, 50f32].into()),
-                "Don't worry!".into(),
-                11,
-                false,
-            ),
-        ],
-        show_errors: true,
-        error: "Illusion".into(),
-        text_placeholder: "Click on what you see".into(),
-        text_win: "You are in security".into(),
-    });
-}
-
-// Note the usage of `ResMut`. Even though `ctx` method doesn't require
-// mutability, accessing the context from different threads will result
-// into panic if you don't enable `egui/multi_threaded` feature.
-fn ui_example(egui_context: ResMut<EguiContext>, mut state: ResMut<GameState>, def: Res<LevelDef>) {
-    egui::Area::new("Hello2")
-        .fixed_pos(egui::Pos2::new(10f32, 10f32))
-        .show(egui_context.ctx(), |ui| {
-            let mut is_correct_click = false;
-            for b in def.buttons.iter() {
-                ui.allocate_ui_at_rect(b.0, |ui| {
-                    if b.3 {
-                        if b.2 == state.success
-                            && ui.button(b.1.to_string()).is_pointer_button_down_on()
-                            && ui.input().pointer.any_pressed()
-                        {
-                            state.success += 1;
-                            is_correct_click = true;
-                        }
-                    } else if b.2 <= state.success
-                        && state.success - b.2 < b.1.len()
-                        && ui
-                            .button(
-                                b.1.chars()
-                                    .skip(state.success - b.2)
-                                    .take(1)
-                                    .collect::<String>(),
-                            )
-                            .is_pointer_button_down_on()
-                        && ui.input().pointer.any_pressed()
-                    {
-                        state.success += 1;
-                        is_correct_click = true;
-                    }
-                });
-            }
-            if !is_correct_click && ui.input().pointer.any_pressed() {
-                if let Some(position) = ui.input().pointer.press_origin() {
-                    state.errors.push(position);
-                }
-            }
-            ui.allocate_ui_at_rect(
-                egui::Rect::from_min_size([0f32, 50f32].into(), [200f32, 50f32].into()),
-                |ui| {
-                    ui.horizontal(|ui| {
-                        ui.colored_label(
-                            egui::Color32::BLUE,
-                            def.text_win.chars().take(state.success).collect::<String>(),
-                        );
-                        ui.colored_label(
-                            egui::Color32::WHITE,
-                            def.text_placeholder
-                                .chars()
-                                .skip(state.success)
-                                .collect::<String>(),
-                        );
-                    })
-                },
-            );
-            if def.show_errors {
-                ui.colored_label(
-                    egui::Color32::YELLOW,
-                    def.error
-                        .chars()
-                        .take(state.errors.len())
-                        .collect::<String>(),
-                );
-            }
-        });
+        .insert(UserInput::default())
+        .insert(Blinking {
+            displayed: 0.33f32,
+            hidden: 1f32,
+        })
+        .insert(BlinkState::Visible(
+            time.time_since_startup().as_secs_f32() + 2f32,
+        ))
+        .insert(MovingDef {
+            path: vec![[200f32, 10f32].into(), [200f32, 100f32].into()],
+            speed: 25f32,
+        })
+        .insert(MovingState { target_index: 0 });
 }
